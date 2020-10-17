aa23f97ac5252b80e70249543580e5904c7344fd
Jose Lopez <joselopezbusiness@gmail.com>
2020-10-17 16:39:14 -0700

    Fixed 3 units Ping Sensor program

--- Ping-3U-Arduino/Ping-3U-Arduino.ino
+++ Ping-3U-Arduino/Ping-3U-Arduino.ino
@@ -2,18 +2,21 @@
 
 const int trigPin1 = 6;
 const int echoPin1 = 7;
+const int trigPin2 = 2;
+const int echoPin2 = 3;
 const int trigPin3 = 10;
 const int echoPin3 = 11;
 
 
 const int echoTime = 7000; //If the Servo isn't working well this is most likely where the error is.
 
-int distance1;
-int distance3;
+int distance1=0;
+int distance2=0;
+int distance3=0;
 
 
 //This sets the samples to average, originally set to 20
-int samples = 20;
+int samples = 40;
 
 
 
@@ -24,8 +27,8 @@ void setup()
   pinMode(trigPin1, OUTPUT);
   pinMode(echoPin1, INPUT);
 
-  //pinMode(trigPin2,OUTPUT);
-  //pinMode(echoPin2,INPUT);
+  pinMode(trigPin2,OUTPUT);
+  pinMode(echoPin2,INPUT);
 
   pinMode(trigPin3, OUTPUT);
   pinMode(echoPin3, INPUT);
@@ -38,18 +41,18 @@ void setup()
 //Here we find the distance measurement in centimeters measured by the Ping Sensors
 void loop() {
   distance1 = (sonicSensor(trigPin1, echoPin1));
-  delay(33);
-  //distance2 = (sonicSensor(trigPin2, echoPin2));
-  //delay(33);
+  
+  distance2 = (sonicSensor(trigPin2, echoPin2));
+  //delay(50); //Adding this line causes issues to the program. 
   distance3 = (sonicSensor(trigPin3, echoPin3));
+
   
 
-
+  
   Serial.print(distance1);
   Serial.print(" ");
-  
-  //Serial.print(distance2);
-  //Serial.println(" ");
+  Serial.print(distance2);
+  Serial.print(" ");
   
   Serial.print(distance3);
   Serial.println(" ");
@@ -60,7 +63,7 @@ void loop() {
 int sonicSensor(int trigPin, int echoPin)
 {
   int distSum;
-  int duration;
+  long duration;
   int dAverage = 0;
   int distance;
   for (int i = 0; i < samples; i++)
@@ -74,7 +77,7 @@ int sonicSensor(int trigPin, int echoPin)
     duration= pulseIn(echoPin, HIGH, echoTime);
     distance = (duration/2)/29.1;
     distSum+= distance;
-    delay(2);
+    //delay(1);
 
   }
 
