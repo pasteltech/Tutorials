3fc523936af4878de5a4e5fa316f9f5a07813c50
Jose Lopez <joselopezbusiness@gmail.com>
2020-10-27 21:12:00 -0700

    Standardized filenames

---
+++ Piezo_1_Basic_Tone/Piezo/Piezo.ino
@@ -0,0 +1,20 @@
+//Use a 2N3904 Transistor
+
+
+const int buzzer = 9; //buzzer to arduino pin 9
+
+
+void setup(){
+ 
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
+
+}
+
+void loop(){
+  
+  tone(buzzer, 1000); // Send 1KHz tone...
+  delay(1000);        // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+  delay(1000);        // ...for 1sec
+  
+}
